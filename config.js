833be550098576a6ce7d6fc0fdddae88a3a972b1
amela.vatre@stu.ibu.edu.ba <amela.vatres@stu.ibu.edu.ba>
2019-06-12 22:54:12 +0200

    Livedb

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secret': 'OvoJeNajjaciSecretKojiMiPadeNaPamet##1337##',
-    //'database': 'mongodb://localhost:27017/EmployeeT',
-    'livedb': 'mongodb://<dbuser>:<dbpassword>@ds033170.mlab.com:33170/employeetracker'
+    'database': 'mongodb://localhost:27017/EmployeeT',
+    'livedb': 'mongodb://amvatres:seproject123@ds033170.mlab.com:33170/employeetracker'
 
 };
